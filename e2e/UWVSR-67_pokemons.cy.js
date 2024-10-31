6ffd8667ef1b778db9d7562731f56d240a3c012d
maratnasretdinov <49758564+maratnasretdinov@users.noreply.github.com>
2024-10-31 17:20:25 +0300

    All

--- e2e/UWVSR-67_pokemons.cy.js
+++ e2e/UWVSR-67_pokemons.cy.js
@@ -1,16 +1,17 @@
 describe('Логин и пароль', function () {
 
    it('Верный пароль и верный логин', function () {
-        cy.visit('https://login.qa.studio/'); // зашел на сайт
-        cy.get('#mail').type('german@dolnikov.ru'); // ввел правильный логин в инпут
-        cy.get('#pass').type('iLoveqastudio1'); // ввел правильный пароль в инпут
-        cy.get('#loginButton').click(); //клик по кнопке 
-        cy.get('#messageHeader').contains('Авторизация прошла успешно'); // проверка наличия подтверждения успешной авторизации
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и его видно
-        cy.get('#exitMessageButton > .exitIcon').click() // клик по крестику, чтобы вернуться в исходное состояние    
+        cy.visit('https://pokemonbattle.ru/shop/'); // зашел на сайт
+        // cy.get('#mail').type('german@dolnikov.ru'); // ввел правильный логин в инпут
+        // cy.get('#pass').type('iLoveqastudio1'); // ввел правильный пароль в инпут
+        // cy.get('#loginButton').click(); //клик по кнопке 
+        // cy.get('#messageHeader').contains('Авторизация прошла успешно'); // проверка наличия подтверждения успешной авторизации
+        // cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и его видно
+        // cy.get('#exitMessageButton > .exitIcon').click() // клик по крестику, чтобы вернуться в исходное состояние    
   
     })
 
  
 })
 // запуск через теринал: npx cypress run --spec cypress/e2e/poke.cy.js --browser chrome
+// npx cypress open
